8d5d4332cd1ea4d466f3627fcc8967d845ecb97f
Stian Soiland-Reyes <soiland-reyes@manchester.ac.uk>
2024-06-26 16:31:56 +0100

    rudimentary content negotiation

---
+++ lib/src/main/java/org/fairdo/benchmark/signposting/SignpostingBitstreamRef.java
@@ -0,0 +1,63 @@
+package org.fairdo.benchmark.signposting;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.Builder;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Optional;
+
+import org.fairdo.benchmark.api.BitstreamRef;
+import org.fairdo.benchmark.api.ContentType;
+import org.fairdo.benchmark.api.ContentType.IANAMediaType;
+import org.fairdo.benchmark.api.PID;
+import org.fairdo.benchmark.api.PID.URIPID;
+
+public class SignpostingBitstreamRef implements BitstreamRef {
+
+	private URIPID pid;
+	private ContentType contentType;
+
+	public SignpostingBitstreamRef(URIPID pid, ContentType contentType) {
+		this.pid = pid;
+	}
+
+	@Override
+	public InputStream asInputStream() throws IOException {
+		Builder builder = HttpRequest.newBuilder().uri(pid.asURI()).GET();
+		String mediaType = contentType().mediaType();
+		if (! mediaType.equals(ContentType.APPLICATION_OCTET_STREAM)) {
+			// Basic content negotiation
+			builder.header("Accept", mediaType);		
+		}	
+		for (PID p : contentType().profiles()) {
+			// Experimental implementation of
+			// https://datatracker.ietf.org/doc/html/draft-svensson-profiled-representations-00
+			builder.header("Accept-Profile", p.asURI().toASCIIString());
+		}		
+		
+		HttpRequest req = builder.build(); 
+		try {
+			HttpResponse<InputStream> resp = HttpClient.newBuilder().followRedirects(Redirect.ALWAYS).build().send(req,
+					BodyHandlers.ofInputStream());
+			return resp.body();
+		} catch (InterruptedException e) {
+			throw new IOException(e);
+		}		
+	}
+
+	@Override
+	public Optional<URI> asURI() throws IOException {
+		return Optional.of(pid.asURI());
+	}
+
+	@Override
+	public ContentType contentType() throws IOException {
+		return contentType;
+	}
+
+}
